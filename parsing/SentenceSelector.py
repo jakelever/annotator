e85302d48b0f97c1ad789e79d404177476c48003
Jake Lever <jlever@bcgsc.ca>
2017-02-21 23:30:32 -0800

    Added a couple extra cases for smart merging

--- parsing/SentenceSelector.py
+++ parsing/SentenceSelector.py
@@ -259,7 +259,7 @@ def selectSentences(entityRequirements, detectFusionGenes, detectMicroRNA, detec
 					specialMerge = False
 					replacementID = None
 					if not idsShared:
-						specialCases = [('snv',['mutation','somatic mutation','mutant','mutated']), ('polymorphism',['polymorphism'])]
+						specialCases = [('snv',['mutation','somatic mutation','mutant','mutated','variant']), ('polymorphism',['polymorphism','snp','variant'])]
 
 						idsA_list,idsB_list = list(idsA),list(idsB)
 						termsA_flatten = " ".join(termsA).lower()
