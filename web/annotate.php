10842ffddde78c5612f752fb7e6d4a10fdb34f25
Jake Lever <jake.lever@gmail.com>
2017-05-29 23:17:15 -0700

    Use full bootstrap (not min)

--- web/annotate.php
+++ web/annotate.php
@@ -264,7 +264,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script>
     <script src="js/typeahead.js"></script>
     <script src="my.js"></script>
   </body>
